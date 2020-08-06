7b64b3e8e472ffd8ca2198697ec258c41c4082d9
Carolina Cunha <36511459+13caroline@users.noreply.github.com>
2020-08-06 15:18:19 +0100

    Update UsersController.cs

--- API/trial2/Controllers/UsersController.cs
+++ API/trial2/Controllers/UsersController.cs
@@ -42,7 +42,7 @@ namespace trial2.Controllers
             return users;
         }
 
-        // GET: api/Users/5
+        // GET: api/Users/Email
         [HttpGet("{id}")]
         public async Task<ActionResult<User>> GetUser(string id)
         {
@@ -62,9 +62,7 @@ namespace trial2.Controllers
 
 
 
-        // PUT: api/Users/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        // PUT: api/Users/Email
         [HttpPut("{id}")]
         public async Task<IActionResult> PutUser(string id, User user)
         {
@@ -97,8 +95,6 @@ namespace trial2.Controllers
         }
 
         // POST: api/Users
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
         public async Task<ActionResult<User>> PostUser(User user)
         {
@@ -124,7 +120,7 @@ namespace trial2.Controllers
             return CreatedAtAction(nameof(GetUser), new { id = user.email }, user);
         }
 
-        // DELETE: api/Users/5
+        // DELETE: api/Users/Email
         [HttpDelete("{id}")]
         public async Task<ActionResult<User>> DeleteUser(string id)
         {
