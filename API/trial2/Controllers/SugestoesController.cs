273224cffa09812f0d4ac885c2ebf946a4e786f2
Carolina Cunha <36511459+13caroline@users.noreply.github.com>
2020-08-06 15:17:01 +0100

    Update SugestoesController.cs

--- API/trial2/Controllers/SugestoesController.cs
+++ API/trial2/Controllers/SugestoesController.cs
@@ -29,7 +29,7 @@ namespace trial2.Controllers
             return await _context.Sugestoes.ToListAsync();
         }
 
-        // GET: api/Sugestoes/5
+        // GET: api/Sugestoes/id
         [HttpGet("{id}")]
         public async Task<ActionResult<Sugestoes>> GetSugestoes(int id)
         {
@@ -45,7 +45,7 @@ namespace trial2.Controllers
             return sugestoes;
         }
 
-        // GET: api/Sugestoes/Users/trial@example.com
+        // GET: api/Sugestoes/Users/Email
         [HttpGet("Users/{id}")]
         public async Task<ActionResult<List<Sugestoes>>> GetSugestoes(string id)
         {
@@ -61,9 +61,7 @@ namespace trial2.Controllers
             return sugestoes;
         }
 
-        // PUT: api/Sugestoes/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        // PUT: api/Sugestoes/id
         [HttpPut("{id}")]
         public async Task<IActionResult> PutSugestoes(int id, Sugestoes sugestoes)
         {
@@ -94,8 +92,6 @@ namespace trial2.Controllers
         }
 
         // POST: api/Sugestoes
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
         public async Task<ActionResult<Sugestoes>> PostSugestoes(Sugestoes sugestoes)
         {
@@ -114,7 +110,7 @@ namespace trial2.Controllers
             return CreatedAtAction("GetSugestoes", new { id = sugestoes.identificacao }, sugestoes);
         }
 
-        // DELETE: api/Sugestoes/5
+        // DELETE: api/Sugestoes/id
         [HttpDelete("{id}")]
         public async Task<ActionResult<Sugestoes>> DeleteSugestoes(int id)
         {
